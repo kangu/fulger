77efb3acc8be5d5077b3d334205bcc4463ec7f72
Radu Stanciu <radu@kangu.ro>
2023-03-28 16:15:08 +0300

    prevent change in invoice amount

--- couchapps/interfaces/validate_doc_update.js
+++ couchapps/interfaces/validate_doc_update.js
@@ -1,6 +1,7 @@
 function (newDoc, oldDoc, userCtx) {
    if (typeof newDoc.LN_Invoice !== "undefined") {
        if (typeof newDoc.ln_invoice_sats !== "number") { throw({forbidden: "Must have sats amount"}) }
+       if (oldDoc && oldDoc.ln_invoice_sats && (oldDoc.ln_invoice_sats !== newDoc.ln_invoice_sats)) { throw({forbidden: "Cannot change amount"}) }
    }
    return true
 }
